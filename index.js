72943f3c4171ef6d0a6d0dd49a8602c6b1970954
Jacob <jacobpayne007@gmail.com>
2024-04-22 23:45:09 -0500

    Done

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Add your code here
+function submitData(name, email) {
+    return fetch("http://localhost:3000/users", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json",
+  },
+  body: JSON.stringify({
+    name: `${name}`,
+    email: `${email}`,
+  }),
+  
+})
+.then(function(response){
+    return response.json();
+})
+.then(function (object) {
+    const idElement = document.createElement('p');
+      const idText = document.createTextNode(`ID: ${object.id}`);
+      idElement.appendChild(idText);
+      document.body.appendChild(idElement);
+  })
+  .catch(function (error) {
+    alert("Bad things! Ragnarők!");
+    console.log(error.message);
+    const errorElement = document.createElement('p');
+      const errorText = document.createTextNode(`Error: ${error.message}`);
+      errorElement.appendChild(errorText);
+      document.body.appendChild(errorElement);
+  });
+}
+;
